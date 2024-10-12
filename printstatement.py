fb56068ccb52fc1956a8ff3f5a6dab585f23eddb
sanan005 <sananmarakkar19@gmail.com>
2024-10-12 20:07:20 +0400

    Add files via upload
    hello , this is print statement activity submitted by cs  year 1 - sanan marakkar

---
+++ printstatement.py
@@ -0,0 +1,61 @@
+#Basic string output
+print("Hello World")
+
+#Simple math inside print
+print("print(5+3)")
+
+#multiple arguments in print
+print("python","is","fun")
+
+#print with escape charecters
+print("she said,\"Hello!\"")
+
+#newline charecter
+print("first line\nSecond line")
+
+#Tab charecter
+print("item1\titem 2\titem 3")
+
+#print multiple data types together
+print("the result is", 42)
+
+#print a single number
+print(12345)
+
+#print a decimal number
+print(3.14159)
+
+#print special charecters
+print("dollar sign:$")
+
+#print using sep parameter
+print("apple","banana","cherry",sep=",")
+
+#print using end parameter
+print("hello",end=" ")
+
+#print a repeated string
+print("python"*3)
+
+#print a unicode charecter
+print("u2764") #prints a heart symbol
+
+#print an empty line
+print()
+
+#concatenation strings inside print
+print("hello"+"world")
+
+#print boolean values
+print(True)
+print(False)
+
+#printing exponential notation
+print(1.2e3)
+#prints 1200.0
+
+#prints multiline using textmusing triple quotes
+print("""this is a multiline print statement""")
+
+#print using an excaped backlash
+print("this is a backlash:\\")
